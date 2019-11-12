54db3ed7da7afdd6a48bf63a6a9f21b14153ed68
Janynne Gomes <janynnegomes@gmail.com>
2019-11-11 23:06:25 -0300

    Module, routing and base component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodoListComponent } from './features/todo-list/todo-list.component';
+import { TodoListModule } from './features/todo-list/todo-list.module';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'todo-list',
+    pathMatch:'full'
+  },
+  {
+      path:'todo-list',
+      component: TodoListComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), TodoListModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
